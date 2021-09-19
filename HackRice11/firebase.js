88bd40f1da7d8ab84acbca9cdefd0858c768a64d
RiceMen57 <90974132+RiceMen57@users.noreply.github.com>
2021-09-19 12:34:07 +0530

    Add files via upload

---
+++ HackRice11/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDO_vFLB0q22n4RxB2G-7wa0up4WWQBnzw",
+    authDomain: "hackrice-11.firebaseapp.com",
+    projectId: "hackrice-11",
+    storageBucket: "hackrice-11.appspot.com",
+    messagingSenderId: "537073614718",
+    appId: "1:537073614718:web:07dd31a93ef64404cd7d2f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
